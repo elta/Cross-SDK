99d4c7950f8bef369c5ca6680109b6f9e96f056b
Jia Liu <proljc@gmail.com>
2013-02-13 19:20:52 +0800

    remove unused part from host-tools

--- scripts/host-tools.sh
+++ scripts/host-tools.sh
@@ -6,111 +6,45 @@ source source.env
 [ -d "${METADATAHOSTTOOLS}" ] || mkdir -p "${METADATAHOSTTOOLS}"
 
 pushd ${SRCHOSTTOOLS}
-[ -f ${METADATAHOSTTOOLS}/autoconf_extract ] || \
-  tar xvf ${TARBALL}/autoconf-${AUTOCONF_VERSION}.${AUTOCONF_SUFFIX} || \
-    die "extract autoconf error" && \
-      touch ${METADATAHOSTTOOLS}/autoconf_extract
-[ -f ${METADATAHOSTTOOLS}/automake_extract ] || \
-  tar vxf ${TARBALL}/automake-${AUTOMAKE_VERSION}.${AUTOMAKE_SUFFIX} || \
-    die "extract automake error" && \
-      touch ${METADATAHOSTTOOLS}/automake_extract
-[ -f ${METADATAHOSTTOOLS}/bash_extract ] || \
-  tar vxf ${TARBALL}/bash-${BASH_VERSION}.${BASH_SUFFIX} || \
-    die "extract bash error" && \
-      touch ${METADATAHOSTTOOLS}/bash_extract
 [ -f ${METADATAHOSTTOOLS}/coreutils_extract ] || \
   tar vxf ${TARBALL}/coreutils-${COREUTILS_VERSION}.${COREUTILS_SUFFIX} || \
     die "extract coreutils error" && \
       touch ${METADATAHOSTTOOLS}/coreutils_extract
+
 [ -f ${METADATAHOSTTOOLS}/gawk_extract ] || \
   tar vxf ${TARBALL}/gawk-${GAWK_VERSION}.${GAWK_SUFFIX} || \
     die "extract gawk error" && \
       touch ${METADATAHOSTTOOLS}/gawk_extract
-[ -f ${METADATAHOSTTOOLS}/e2fs_extract ] || \
-  tar vxf ${TARBALL}/e2fsprogs-${E2FSPROGS_VERSION}.${E2FSPROGS_SUFFIX} || \
-    die "extract e2fs error" && \
-      touch ${METADATAHOSTTOOLS}/e2fs_extract
+
 [ -f ${METADATAHOSTTOOLS}/gettext_extract ] || \
   tar vxf ${TARBALL}/gettext-${GETTEXT_VERSION}.${GETTEXT_SUFFIX} || \
     die "extract gettext error" && \
       touch ${METADATAHOSTTOOLS}/gettext_extract
-[ -f ${METADATAHOSTTOOLS}/grep_extract ] || \
-  tar vxf ${TARBALL}/grep-${GREP_VERSION}.${GREP_SUFFIX} || \
-    die "extract grep error" && \
-      touch ${METADATAHOSTTOOLS}/grep_extract
-[ -f ${METADATAHOSTTOOLS}/libiconv_extract ] || \
-  tar xvf ${TARBALL}/libiconv-${LIBICONV_VERSION}.${LIBICONV_SUFFIX} || \
-    die "extract libiconv error" && \
-      touch ${METADATAHOSTTOOLS}/libiconv_extract
-[ -f ${METADATAHOSTTOOLS}/libtool_extract ] || \
-  tar vxf ${TARBALL}/libtool-${LIBTOOL_VERSION}.${LIBTOOL_SUFFIX} || \
-    die "extract libtool error" && \
-      touch ${METADATAHOSTTOOLS}/libtool_extract
+
 [ -f ${METADATAHOSTTOOLS}/make_extract ] || \
   tar vxf ${TARBALL}/make-${MAKE_VERSION}.${MAKE_SUFFIX} || \
     die "extract make error" && \
       touch ${METADATAHOSTTOOLS}/make_extract
+
 [ -f ${METADATAHOSTTOOLS}/sed_extract ] || \
   tar vxf ${TARBALL}/sed-${SED_VERSION}.${SED_SUFFIX} || \
     die "extract sed error" && \
       touch ${METADATAHOSTTOOLS}/sed_extract
+
 [ -f ${METADATAHOSTTOOLS}/texinfo_extract ] || \
   tar vxf ${TARBALL}/texinfo-${TEXINFO_VERSION}a.${TEXINFO_SUFFIX} || \
     die "extract texinfo error" && \
       touch ${METADATAHOSTTOOLS}/texinfo_extract
-[ -f ${METADATAHOSTTOOLS}/zlib_extract ] || \
-  tar vxf ${TARBALL}/zlib-${ZLIB_VERSION}.${ZLIB_SUFFIX} || \
-    die "extract zlib error" && \
-      touch ${METADATAHOSTTOOLS}/zlib_extract
-popd
 
-pushd ${SRCHOSTTOOLS}
-cd autoconf-${AUTOCONF_VERSION}/
-[ -f ${METADATAHOSTTOOLS}/autoconf_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure autoconf error" && \
-      touch ${METADATAHOSTTOOLS}/autoconf_configure
-[ -f ${METADATAHOSTTOOLS}/autoconf_build ] || \
-  make -j${JOBS} || \
-    die "build autoconf error" && \
-      touch ${METADATAHOSTTOOLS}/autoconf_build
-[ -f ${METADATAHOSTTOOLS}/autoconf_install ] || \
-  make install || \
-    die "install autoconf error" && \
-      touch ${METADATAHOSTTOOLS}/autoconf_install
-popd
+[ -f ${METADATAHOSTTOOLS}/perl_extract ] || \
+  tar xf ${TARBALL}/perl-${PERL_VERSION}.${PERL_SUFFIX} || \
+    die "***extract perl error" && \
+      touch ${METADATAHOSTTOOLS}/perl_extract
 
-pushd ${SRCHOSTTOOLS}
-cd automake-${AUTOMAKE_VERSION}
-[ -f ${METADATAHOSTTOOLS}/automake_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure automake error" && \
-      touch ${METADATAHOSTTOOLS}/automake_configure
-[ -f ${METADATAHOSTTOOLS}/automake_build ] || \
-  make -j${JOBS} || \
-    die "build automake error" && \
-      touch ${METADATAHOSTTOOLS}/automake_build
-[ -f ${METADATAHOSTTOOLS}/automake_install ] || \
-  make install || \
-    die "install automake error" && \
-      touch ${METADATAHOSTTOOLS}/automake_install
-popd
-
-
-pushd ${SRCHOSTTOOLS}
-cd bash-${BASH_VERSION}
-[ -f ${METADATAHOSTTOOLS}/bash_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure bash error" && \
-      touch ${METADATAHOSTTOOLS}/bash_configure
-[ -f ${METADATAHOSTTOOLS}/bash_build ] || \
-  make -j${JOBS} || \
-    die "build bash error" && \
-      touch ${METADATAHOSTTOOLS}/bash_build
-[ -f ${METADATAHOSTTOOLS}/bash_install ] || \
-  make install || \
-    die "install bash error" && \
-      touch ${METADATAHOSTTOOLS}/bash_install
+[ -f ${METADATAHOSTTOOLS}/python_extract ] || \
+  tar xf ${TARBALL}/python-${PYTHON_VERSION}.${PYTHON_SUFFIX} || \
+    die "***extract python error" && \
+      touch ${METADATAHOSTTOOLS}/python_extract
 popd
 
 pushd ${SRCHOSTTOOLS}
@@ -150,22 +84,6 @@ cd gawk-${GAWK_VERSION}
 popd
 
 pushd ${SRCHOSTTOOLS}
-cd e2fsprogs-${E2FSPROGS_VERSION}
-[ -f ${METADATAHOSTTOOLS}/e2fs_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} --sbin=${PREFIXHOSTTOOLS}/bin || \
-    die "configure e2fs error" && \
-      touch ${METADATAHOSTTOOLS}/e2fs_configure
-[ -f ${METADATAHOSTTOOLS}/e2fs_build ] || \
-  make -j${JOBS} || \
-    die "build e2fs error" && \
-      touch ${METADATAHOSTTOOLS}/e2fs_build
-[ -f ${METADATAHOSTTOOLS}/e2fs_install ] || \
-  make install || \
-    die "install e2fs error" && \
-      touch ${METADATAHOSTTOOLS}/e2fs_install
-popd
-
-pushd ${SRCHOSTTOOLS}
 cd gettext-${GETTEXT_VERSION}
 [ -f ${METADATAHOSTTOOLS}/gettext_configure ] || \
   ./configure --prefix=${PREFIXHOSTTOOLS} || \
@@ -179,38 +97,6 @@ cd gettext-${GETTEXT_VERSION}
   make install || \
     die "install gettext error" && \
       touch ${METADATAHOSTTOOLS}/gettext_install
-popd
-
-pushd ${SRCHOSTTOOLS}
-cd grep-${GREP_VERSION}
-[ -f ${METADATAHOSTTOOLS}/grep_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure grep error" && \
-      touch ${METADATAHOSTTOOLS}/grep_configure
-[ -f ${METADATAHOSTTOOLS}/grep_build ] || \
-  make -j${JOBS} || \
-    die "build grep error" && \
-      touch ${METADATAHOSTTOOLS}/grep_build
-[ -f ${METADATAHOSTTOOLS}/grep_install ] || \
-  make install || \
-    die "install grep error" && \
-      touch ${METADATAHOSTTOOLS}/grep_install
-popd
-
-pushd ${SRCHOSTTOOLS}
-cd libtool-${LIBTOOL_VERSION}
-[ -f ${METADATAHOSTTOOLS}/libtool_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure libtool error" && \
-      touch ${METADATAHOSTTOOLS}/libtool_configure
-[ -f ${METADATAHOSTTOOLS}/libtool_build ] || \
-  make -j${JOBS} || \
-    die "build libtool error" && \
-      touch ${METADATAHOSTTOOLS}/libtool_build
-[ -f ${METADATAHOSTTOOLS}/libtool_install ] || \
-  make install || \
-    die "install libtool error" && \
-      touch ${METADATAHOSTTOOLS}/libtool_install
 popd
 
 pushd ${SRCHOSTTOOLS}
@@ -259,36 +145,4 @@ cd texinfo-${TEXINFO_VERSION}
   make install || \
     die "install texinfo error" && \
       touch ${METADATAHOSTTOOLS}/texinfo_install
-popd
-
-pushd ${SRCHOSTTOOLS}
-cd zlib-${ZLIB_VERSION}
-[ -f ${METADATAHOSTTOOLS}/zlib_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure zlib error" && \
-      touch ${METADATAHOSTTOOLS}/zlib_configure
-[ -f ${METADATAHOSTTOOLS}/zlib_build ] || \
-  make -j${JOBS} || \
-    die "build zlib error" && \
-      touch ${METADATAHOSTTOOLS}/zlib_build
-[ -f ${METADATAHOSTTOOLS}/zlib_install ] || \
-  make install || \
-    die "install zlib error" && \
-      touch ${METADATAHOSTTOOLS}/zlib_install
-popd
-
-pushd ${SRCHOSTTOOLS}
-cd libiconv-${LIBICONV_VERSION}
-[ -f ${METADATAHOSTTOOLS}/libiconv_configure ] || \
-  ./configure --prefix=${PREFIXHOSTTOOLS} || \
-    die "configure libiconv error" && \
-      touch ${METADATAHOSTTOOLS}/libiconv_configure
-[ -f ${METADATAHOSTTOOLS}/libiconv_build ] || \
-  make -j${JOBS} || \
-    die "build libiconv error" && \
-      touch ${METADATAHOSTTOOLS}/libiconv_build
-[ -f ${METADATAHOSTTOOLS}/libiconv_install ] || \
-  make install || \
-    die "install libiconv error" && \
-      touch ${METADATAHOSTTOOLS}/libiconv_install
 popd
