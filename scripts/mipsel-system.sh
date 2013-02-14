e4dd415c1b557250a34ed5b6ac4a7e97eba6729d
Jia Liu <proljc@gmail.com>
2013-02-14 14:53:16 +0800

    rename

--- scripts/mipsel-system.sh
+++ scripts/mipsel-system.sh
@@ -6,15 +6,18 @@ source source.env
   source host-tools.step || \
     die "build host-tools error" && \
       touch ${METADATAHOSTTOOLS}/host_tools_finished
+
 [ -f "${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished" ] || \
   source mipsel-linux-tool.step || \
     die "build mipsel tools error" && \
       touch ${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished
+
 [ -f "${METADATAMIPSELKERNEL}/linux_kernel_finished" ] || \
-  source mipsel-linux-kernel.step || \
+  source mipsel-linux.step || \
     die "build mipsel linux kernel and modules error" && \
       touch ${METADATAMIPSELKERNEL}/linux_kernel_finished
+
 [ -f "${METADATAMIPSELROOTFS}/mipsel_rootfs_finished" ] || \
-  source mipsel-linux-rootfs.step || \
+  source mipsel-gnu-rootfs.step || \
     die "build mipsel rootfs error" && \
       touch ${METADATAMIPSELROOTFS}/mipsel_rootfs_finished
