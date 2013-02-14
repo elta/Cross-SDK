128d5600ff712900acc5493bcf34cb2668c45272
Jia Liu <proljc@gmail.com>
2013-02-14 14:38:37 +0800

    add qtc into toolchain

--- scripts/mipsel-toolchain.sh
+++ scripts/mipsel-toolchain.sh
@@ -6,15 +6,23 @@ source source.env
   source host-tools.step || \
     die "build host-tools error" && \
       touch ${METADATAHOSTTOOLS}/host_tools_finished
+
 [ -f "${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished" ] || \
   source mipsel-linux-tool.step || \
     die "build mipsel tools error" && \
       touch ${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished
-[ -f "${METADATAUNIVERSAL}/tool_llvm_finished" ] || \
+
+[ -f "${METADATAUNIVERSAL}/mips_llvm_finished" ] || \
   source mips-llvm.step || \
     die "build llvm error" && \
-      touch ${METADATAUNIVERSAL}/tool_llvm_finished
-[ -f "${METADATAUNIVERSAL}/tool_mips_qemu_finished" ] || \
+      touch ${METADATAUNIVERSAL}/mips_llvm_finished
+
+[ -f "${METADATAUNIVERSAL}/mips_qemu_finished" ] || \
   source mips-qemu.step || \
     die "build mips qemu error" && \
-      touch ${METADATAUNIVERSAL}/tool_mips_qemu_finished
+      touch ${METADATAUNIVERSAL}/mips_qemu_finished
+
+[ -f "${METADATAUNIVERSAL}/qtc_finished" ] || \
+  source qtc.step || \
+    die "build qt creator error" && \
+      touch ${METADATAUNIVERSAL}/qtc_finished
