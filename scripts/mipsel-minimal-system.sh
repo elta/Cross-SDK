56464b6d7006a95f6e77949fa97bedec4ec271c0
Dongxue Zhang <elta.era@gmail.com>
2013-02-14 14:19:55 +0800

    Add mipsel-minimal-system.sh, which use busybox as filesystem.

---
+++ scripts/mipsel-minimal-system.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+source source.env
+
+[ -f "${METADATAHOSTTOOLS}/host_tools_finished" ] || \
+  source host-tools.step || \
+    die "build host-tools error" && \
+      touch ${METADATAHOSTTOOLS}/host_tools_finished
+[ -f "${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished" ] || \
+  source mipsel-linux-tool.step || \
+    die "build mipsel tools error" && \
+      touch ${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished
+[ -f "${METADATAMIPSELKERNEL}/linux_kernel_finished" ] || \
+  source mipsel-linux-kernel.step || \
+    die "build mipsel linux kernel and modules error" && \
+      touch ${METADATAMIPSELKERNEL}/linux_kernel_finished
+[ -f "${METADATABUSYBOXO32}/mipsel_busybox_finished" ] || \
+  source mipsel-busybox.step || \
+    die "build mipsel busybox error" && \
+      touch ${METADATABUSYBOXO32}/mipsel_busybox_finished
