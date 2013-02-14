c72f4d2711b199af94f4f6cc176fbf34874510cb
Dongxue Zhang <elta.era@gmail.com>
2013-02-14 13:59:52 +0800

    change host-tools/mipsel-linux-kernel/mipsel-linux-rootfs/mipsel-linux-tool's suffix from sh to step, as they are step of build a full system. Update mipsel-system.sh where use changed things.

--- scripts/mipsel-system.sh
+++ scripts/mipsel-system.sh
@@ -3,18 +3,18 @@
 source source.env
 
 [ -f "${METADATAHOSTTOOLS}/host_tools_finished" ] || \
-  source host-tools.sh || \
+  source host-tools.step || \
     die "build host-tools error" && \
       touch ${METADATAHOSTTOOLS}/host_tools_finished
 [ -f "${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished" ] || \
-  source mipsel-linux-tool.sh || \
+  source mipsel-linux-tool.step || \
     die "build mipsel tools error" && \
       touch ${METADATAMIPSELTOOLCHAIN}/mipsel_tools_finished
 [ -f "${METADATAMIPSELKERNEL}/linux_kernel_finished" ] || \
-  source mipsel-linux-kernel.sh || \
+  source mipsel-linux-kernel.step || \
     die "build mipsel linux kernel and modules error" && \
       touch ${METADATAMIPSELKERNEL}/linux_kernel_finished
 [ -f "${METADATAMIPSELROOTFS}/mipsel_rootfs_finished" ] || \
-  source mipsel-linux-rootfs.sh || \
+  source mipsel-linux-rootfs.step || \
     die "build mipsel rootfs error" && \
       touch ${METADATAMIPSELROOTFS}/mipsel_rootfs_finished
